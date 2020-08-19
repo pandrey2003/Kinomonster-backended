ba76755a27ed1df0efcd137f48d4e61af4ecd7bf
AndrewPolukhin <qq7039108@gmail.com>
2020-08-19 17:49:06 +0300

    Used .jinja2 as a more reliable option for {% endfor %} blocks

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.jinja2")
 
 
 @app.route("/films")
 def films():
-    return render_template("films.html")
+    return render_template("films.jinja2")
 
 
 @app.route("/serial")
 def serial():
-    return render_template("serial.html")
+    return render_template("serial.jinja2")
 
 
 @app.route("/rating")
 def rating():
-    return render_template("rating.html")
+    return render_template("rating.jinja2")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.jinja2")
 
 
-@app.route("/show")
-def show():
-    return render_template("show.html")
+@app.route("/interstellar")
+def interstellar():
+    return render_template("interstellar.jinja2")
 
 
 if __name__ == "__main__":
