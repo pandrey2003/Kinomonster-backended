41768a5f8731427305a1f59c9ed682fab33c54c1
AndrewPolukhin <qq7039108@gmail.com>
2020-08-30 12:36:26 +0300

    Enabled caching for homepage

--- app/mod_db/posts.py
+++ app/mod_db/posts.py
@@ -2,6 +2,7 @@ from app.mod_db import session, Posts
 from cachetools import cached, TTLCache
 
 cache = TTLCache(maxsize=float('inf'), ttl=900)
+cache_home = TTLCache(maxsize=100, ttl=900)
 
 
 @cached(cache)
@@ -9,6 +10,7 @@ def get_posts():
     return session.query(Posts).all()[::-1]
 
 
+@cached(cache_home)
 def get_posts_for_home():
     posts = get_posts()
     return posts[0:2]
@@ -16,3 +18,4 @@ def get_posts_for_home():
 
 def release_cache():
     cache.clear()
+    cache_home.clear()
