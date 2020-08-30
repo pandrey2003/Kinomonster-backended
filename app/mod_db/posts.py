89eb8d477c1b03b095e5cf7ee982cf968b5e3c84
AndrewPolukhin <qq7039108@gmail.com>
2020-08-30 12:34:49 +0300

    Enabled caching from database

--- app/mod_db/posts.py
+++ app/mod_db/posts.py
@@ -1,6 +1,10 @@
 from app.mod_db import session, Posts
+from cachetools import cached, TTLCache
+
+cache = TTLCache(maxsize=float('inf'), ttl=900)
 
 
+@cached(cache)
 def get_posts():
     return session.query(Posts).all()[::-1]
 
@@ -8,3 +12,7 @@ def get_posts():
 def get_posts_for_home():
     posts = get_posts()
     return posts[0:2]
+
+
+def release_cache():
+    cache.clear()
